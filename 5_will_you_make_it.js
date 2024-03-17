293c7d89114881ef40f317cc04a0ebf1dc687524
MacBook <macbook@192.168.1.10>
2024-03-17 17:08:54 +0700

    Day 5 zeroFuel

---
+++ 5_will_you_make_it.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/javascript
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//     return distanceToPump / mpg <= fuelLeft;
+//   };
+
+const zeroFuel = (distanceToPump, mpg, fuelLeft) => distanceToPump / mpg <= fuelLeft;
+
+
+  console.log(zeroFuel(50, 25, 2));
+  console.log(zeroFuel(100,50,1));
