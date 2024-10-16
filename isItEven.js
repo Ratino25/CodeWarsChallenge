bb6784efb4045b21d76dc68767d719998aa20750
Ratino <ratinoray@gmail.com>
2024-10-16 11:17:21 +0700

    isItEven

---
+++ isItEven.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/555a67db74814aa4ee0001b5/train/javascript
+
+function testEven(n){
+    return n % 2 ===0;
+}
+
+console.log(testEven(2));
+console.log(testEven(3));
+console.log(testEven(4));
+console.log(testEven(5));
