8f3b6381c947b9c124c08352f879da2b9d3d3ef4
Ratino <ratinoray@gmail.com>
2024-10-18 10:32:35 +0700

    Add link on AlternatingCase

--- Alternating.js
+++ Alternating.js
@@ -1,3 +1,4 @@
+// https://www.codewars.com/kata/56efc695740d30f963000557/solutions/javascript
 String.prototype.toAlternatingCase = function () {
     // Define your method here :)
     // return this.split('')
