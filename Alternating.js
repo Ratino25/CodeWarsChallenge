63c6bc6fb3bcb8f6f2a23de0d15e5b5020446f96
Ratino <ratinoray@gmail.com>
2024-10-18 10:27:58 +0700

    AlternatingCase

---
+++ Alternating.js
@@ -0,0 +1,26 @@
+String.prototype.toAlternatingCase = function () {
+    // Define your method here :)
+    // return this.split('')
+    // .map(function (char){
+    //     if(char === char.toUpperCase()){
+    //         return char.toLowerCase();
+    //     } else {
+    //         return char.toUpperCase();
+    //     }
+    // }).join('');
+
+    // Cara 2
+    new_str = '';
+    for(let i = 0; i < this.length; i++){
+        if(this[i] === this[i].toLowerCase()){
+            new_str += this[i].toUpperCase();
+        } else {
+            new_str += this[i].toLowerCase();
+        }
+    }
+    return new_str;
+  }
+
+console.log("Hello World".toAlternatingCase());
+console.log("hello world".toAlternatingCase());
+console.log("HELLO WORLD".toAlternatingCase());
