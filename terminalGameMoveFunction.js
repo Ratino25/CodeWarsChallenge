aa74a3338da51b14b433c164baf11b48d6d85e16
Ratino <ratinoray@gmail.com>
2024-12-09 18:17:45 +0700

    terminal Game move function

---
+++ terminalGameMoveFunction.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/563a631f7cbbc236cf0000c2/train/javascript
+function move(position, roll){
+    return position + roll * 2;
+}
+
+console.log(move(0, 3)); // 6
+
+console.log(move(5, 2)); // 12
+
+console.log(move(10, 1)); // 20
+console.log(move(3,6));
