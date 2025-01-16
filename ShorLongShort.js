a0d298cf34be89846606ec73bb8d9d93a0fee508
Ratino <ratinoray@gmail.com>
2025-01-16 18:13:35 +0700

    ShortLangSort

--- ShorLongShort.js
+++ ShorLongShort.js
@@ -3,7 +3,8 @@
 
 function solution(a, b){
     // your code here
-    
+    return (a.length < b.length) ? a + b + a : b + a + b;
 }
 
 console.log(solution("1", "22"));
+console.log(solution("11", "2"));
