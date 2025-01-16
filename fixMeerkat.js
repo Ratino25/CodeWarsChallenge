663e868fae1387552b2bc1c86861c7dade3ca77f
Ratino <ratinoray@gmail.com>
2025-01-16 14:22:56 +0700

    fixMeerkat

---
+++ fixMeerkat.js
@@ -0,0 +1,6 @@
+function fixTheMeerkat(arr){
+    return arr.reverse();
+}
+
+console.log(fixTheMeerkat(["tail", "body", "head"]));
+console.log(fixTheMeerkat(["lower legs", "torso", "upper legs"]));
