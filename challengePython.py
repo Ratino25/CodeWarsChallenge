edeb78f3a5e7bcfe461e4659df517ecfbd9058e2
Ratino <ratinoray@gmail.com>
2025-02-15 18:22:52 +0700

    drinkAbout

--- challengePython.py
+++ challengePython.py
@@ -1,5 +1,14 @@
-def nth_even(n):
-    return 2 * (n - 1)
-
-print(nth_even(1))
-print(nth_even(3))
+def people_with_age_drink(age):
+    # if age < 14:
+    #     return "drink toddy"
+    # elif age < 18:
+    #     return "drink coke"
+    # elif age < 21:
+    #     return "drink beer"
+    # else:
+    #     return "drink whiskey"
+    
+    return "drink " + ("toddy" if age < 14 else "coke" if age < 18 else "beer" if age < 21 else "whisky")
+    
+print(people_with_age_drink(15)) # "drink coke"
+print(people_with_age_drink(19))
