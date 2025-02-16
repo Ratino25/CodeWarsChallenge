5aa4018131a5801d8e154fb68d1a10d0d9bdb21b
MacBook <macbook@192.168.1.13>
2025-02-16 15:58:07 +0700

    getCharASCII

--- challengePython.py
+++ challengePython.py
@@ -1,14 +1,5 @@
-def people_with_age_drink(age):
-    # if age < 14:
-    #     return "drink toddy"
-    # elif age < 18:
-    #     return "drink coke"
-    # elif age < 21:
-    #     return "drink beer"
-    # else:
-    #     return "drink whiskey"
-    
-    return "drink " + ("toddy" if age < 14 else "coke" if age < 18 else "beer" if age < 21 else "whisky")
-    
-print(people_with_age_drink(15)) # "drink coke"
-print(people_with_age_drink(19))
+def get_char(c):
+    return chr(c)
+
+print(get_char(65))
+print(get_char(66))
