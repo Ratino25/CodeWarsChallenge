99a7c22e1dce18af411754c87505e190ac0fa543
MacBook <macbook@192.168.1.13>
2025-02-22 18:21:38 +0700

    findThePosition

--- challengePython.py
+++ challengePython.py
@@ -1,5 +1,21 @@
-def get_char(c):
-    return chr(c)
+def position(letter):
+    alphabets = "abcdefghijklmnopqrstuvwxyz"
+    index = alphabets.index(letter)
+    return f"Positon of alphabet: {index + 1}"
 
-print(get_char(65))
-print(get_char(66))
+print(position("a"))
+print(position("b"))
+print(position("c"))
+print(position("d"))
+print(position("e"))
+print(position("f"))
+print(position("g"))
+print(position("h"))
+print(position("i"))
+print(position("j"))
+print(position("k"))
+print(position("l"))
+print(position("m"))
+print(position("n"))
+print(position("o"))
+print(position("p"))
