fd751a1152bb890e5e9ef22ac89c5a1fe3bb82a1
MacBook <macbook@192.168.1.13>
2025-03-07 05:48:13 +0700

    reverseString

--- challengePython.py
+++ challengePython.py
@@ -1,9 +1,4 @@
-def mouth_size(animal):
-    if animal.upper() == "ALLIGATOR":
-        return "small"
-    else:
-        return "wide"
-    
-print(mouth_size("ALLIGATOR"))
+def reverse(st):
+    return " ".join(st.split()[::-1])
 
-
+print(reverse("Hello World"))
